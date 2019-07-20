ecc689a3a2817a95a23031b02fe0fcdf642b7803
drdhaval2785 <drdhaval2785@gmail.com>
2019-07-20 11:13:47 +0530

    hwextra from Cologne

--- v00/updateDistincScripts.sh
+++ v00/updateDistincScripts.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 echo "Download orig/xxx.txt to each dictionary's orig folder from live Cologne server to csl-data folder."
 
-dictsthirteen=(BUR INM MWE PWG SKD STC)
-dictsfourteen=(ACC AE AP90 AP BEN BHS BOP BOR CAE CCS GRA GST IEG KRM MCI MD MW72 MW PD PE PGN PUI PW SCH SHS SNP VCP VEI WIL YAT)
-for dict in ${dictsthiteen[*]}
+dictsthirteen=(BUR INM MWE PWG SKD STC VCP)
+dictsfourteen=(ACC AE AP90 AP BEN BHS BOP BOR CAE CCS GRA GST IEG KRM MCI MD MW72 MW PD PE PGN PUI PW SCH SHS SNP VEI WIL YAT)
+for dict in ${dictsthirteen[*]}
 do
 	echo "${dict,,}"
 	mkdir -p csl-data/"$dict"Scan/2020/orig/hwextra
-	#wget -O csl-data/"$dict"Scan/2020/orig/"${dict,,}".txt https://www.sanskrit-lexicon.uni-koeln.de/scans/"$dict"Scan/2013/orig/"${dict,,}".txt
-	cp ../../../Cologne_localcopy/"${dict,,}"/pywork/hwextra/"${dict,,}"_hwextra.txt csl-data/"$dict"Scan/2020/orig/hwextra/"${dict,,}"_hwextra.txt
+	wget -O csl-data/"$dict"Scan/2020/orig/"${dict,,}".txt https://www.sanskrit-lexicon.uni-koeln.de/scans/"$dict"Scan/2013/orig/"${dict,,}".txt
+	wget -O csl-data/"$dict"Scan/2020/orig/hwextra/"${dict,,}"_hwextra.txt https://www.sanskrit-lexicon.uni-koeln.de/scans/"$dict"Scan/2013/pywork/hwextra/"${dict,,}"_hwextra.txt
 done
 
 for dict in ${dictsfourteen[*]}
 do
 	echo "${dict,,}"
 	mkdir -p csl-data/"$dict"Scan/2020/orig/hwextra
-	# wget -O csl-data/"$dict"Scan/2020/orig/"${dict,,}".txt https://www.sanskrit-lexicon.uni-koeln.de/scans/"$dict"Scan/2014/orig/"${dict,,}".txt
-	cp ../../../Cologne_localcopy/"${dict,,}"/pywork/hwextra/"${dict,,}"_hwextra.txt csl-data/"$dict"Scan/2020/orig/hwextra/"${dict,,}"_hwextra.txt
+	wget -O csl-data/"$dict"Scan/2020/orig/"${dict,,}".txt https://www.sanskrit-lexicon.uni-koeln.de/scans/"$dict"Scan/2014/orig/"${dict,,}".txt
+	wget -O csl-data/"$dict"Scan/2020/orig/hwextra/"${dict,,}"_hwextra.txt https://www.sanskrit-lexicon.uni-koeln.de/scans/"$dict"Scan/2014/pywork/hwextra/"${dict,,}"_hwextra.txt
 done
 
