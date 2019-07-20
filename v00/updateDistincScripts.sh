f325e2abde7262036a3e539cd1c0fdc159bcf4e2
drdhaval2785 <drdhaval2785@gmail.com>
2019-07-20 10:45:30 +0530

    added hwextra

--- v00/updateDistincScripts.sh
+++ v00/updateDistincScripts.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-echo "Download unique scripts to each dictionary's pywork from live Cologne server to distinctscripts folder."
-scripts=(make_xml.py)
-dicts=(ACC AE AP90 AP BEN BHS BOP BOR BUR CAE CCS GRA GST IEG INM KRM MCI MD MW72 MWE MW PD PE PGN PUI PWG PW SCH SHS SKD SNP STC VCP VEI WIL YAT)
-for dict in ${dicts[*]}
+echo "Download orig/xxx.txt to each dictionary's orig folder from live Cologne server to csl-data folder."
+
+dictsthirteen=(BUR INM MWE PWG SKD STC)
+dictsfourteen=(ACC AE AP90 AP BEN BHS BOP BOR CAE CCS GRA GST IEG KRM MCI MD MW72 MW PD PE PGN PUI PW SCH SHS SNP VCP VEI WIL YAT)
+for dict in ${dictsthiteen[*]}
 do
-	for script in ${scripts[*]}
-	do
-		echo "${dict,,}"
-		mkdir -p csl-data/"$dict"Scan/2020/orig
-		wget -O csl-data/"$dict"Scan/2020/orig/"${dict,,}".txt https://www.sanskrit-lexicon.uni-koeln.de/scans/"$dict"Scan/2014/orig/"${dict,,}".txt
-	done
+	echo "${dict,,}"
+	mkdir -p csl-data/"$dict"Scan/2020/orig/hwextra
+	#wget -O csl-data/"$dict"Scan/2020/orig/"${dict,,}".txt https://www.sanskrit-lexicon.uni-koeln.de/scans/"$dict"Scan/2013/orig/"${dict,,}".txt
+	cp ../../../Cologne_localcopy/"${dict,,}"/pywork/hwextra/"${dict,,}"_hwextra.txt csl-data/"$dict"Scan/2020/orig/hwextra/"${dict,,}"_hwextra.txt
+done
+
+for dict in ${dictsfourteen[*]}
+do
+	echo "${dict,,}"
+	mkdir -p csl-data/"$dict"Scan/2020/orig/hwextra
+	# wget -O csl-data/"$dict"Scan/2020/orig/"${dict,,}".txt https://www.sanskrit-lexicon.uni-koeln.de/scans/"$dict"Scan/2014/orig/"${dict,,}".txt
+	cp ../../../Cologne_localcopy/"${dict,,}"/pywork/hwextra/"${dict,,}"_hwextra.txt csl-data/"$dict"Scan/2020/orig/hwextra/"${dict,,}"_hwextra.txt
 done
 
